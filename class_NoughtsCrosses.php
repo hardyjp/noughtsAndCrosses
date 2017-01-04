dd836c78c04013b7b68c1a19563c267bc351a0ac
hardyjp <hardyjp@aol.com>
2017-01-04 10:44:29 +0000

    upload class_NoughtsCrosses
    Tidy up

--- class_NoughtsCrosses.php
+++ class_NoughtsCrosses.php
@@ -1,12 +1,17 @@
 <?php
 class NoughtsCrosses
 {
-	public $xWins, $oWins, $draws = 0;
+	public $xWins = 0;
+	public $oWins = 0;
+	public $draws = 0;
+	private $hn = 'localhost';
+	private $db = 'oandx';
+	private $un = 'jph';
+	private $pw = 'consett';
 
 	function get_aggregate_results()
     {
-		require_once 'login.php';
-		$conn = new mysqli($hn, $un, $pw, $db);
+		$conn = new mysqli($this->hn, $this->un, $this->pw, $this->db);
 		if ($conn->connect_error) die($conn->connect_error);
 		$query  = "SELECT result, COUNT(result) FROM results GROUP BY result ORDER BY result DESC";
 		$result = $conn->query($query);
@@ -31,62 +36,63 @@ class NoughtsCrosses
 
 	function calculate_winners()
     {
-		require_once 'login.php';
-		$conn = new mysqli($hn, $un, $pw, $db);
+		$conn = new mysqli($this->hn, $this->un, $this->pw, $this->db);
 		if ($conn->connect_error) die($conn->connect_error);
 		$x = 0;
 		while (FALSE !== ($line = fgets(STDIN))) {
-			$row[$x] = array($line[0],$line[1],$line[2]);
-			if($x < 2){
-				++$x;
-			} else {
-				$square1 = (string)$row[0][0];
-				$square2 = (string)$row[0][1];
-				$square3 = (string)$row[0][2];
-				$square4 = (string)$row[1][0];
-				$square5 = (string)$row[1][1];
-				$square6 = (string)$row[1][2];
-				$square7 = (string)$row[2][0];
-				$square8 = (string)$row[2][1];
-				$square9 = (string)$row[2][2];
-				// eight possible winning lines, otherwise a draw
-				if($square1 == $square2 && $square1 == $square3){
-					$result = $square1;
-				} elseif($square1 == $square4 && $square1 == $square7){
-					$result = $square1;
-				} elseif($square1 == $square5 && $square1 == $square9){
-					$result = $square1;
-				} elseif($square2 == $square5 && $square2 == $square8){
-					$result = $square2;
-				} elseif($square3 == $square5 && $square3 == $square7){
-					$result = $square3;
-				} elseif($square3 == $square6 && $square3 == $square9){
-					$result = $square3;
-				} elseif($square4 == $square5 && $square4 == $square6){
-					$result = $square4;
-				} elseif($square7 == $square8 && $square7 == $square9){
-					$result = $square7;
+			if($line[0] != "\n" && $line[0] != "\r"){
+				$row[$x] = array($line[0],$line[1],$line[2]);
+				if($x < 2){
+					++$x;
 				} else {
-					$result = "D";
+					$square1 = (string)$row[0][0];
+					$square2 = (string)$row[0][1];
+					$square3 = (string)$row[0][2];
+					$square4 = (string)$row[1][0];
+					$square5 = (string)$row[1][1];
+					$square6 = (string)$row[1][2];
+					$square7 = (string)$row[2][0];
+					$square8 = (string)$row[2][1];
+					$square9 = (string)$row[2][2];
+					// eight possible winning lines, otherwise a draw
+					if($square1 == $square2 && $square1 == $square3){
+						$result = $square1;
+					} elseif($square1 == $square4 && $square1 == $square7){
+						$result = $square1;
+					} elseif($square1 == $square5 && $square1 == $square9){
+						$result = $square1;
+					} elseif($square2 == $square5 && $square2 == $square8){
+						$result = $square2;
+					} elseif($square3 == $square5 && $square3 == $square7){
+						$result = $square3;
+					} elseif($square3 == $square6 && $square3 == $square9){
+						$result = $square3;
+					} elseif($square4 == $square5 && $square4 == $square6){
+						$result = $square4;
+					} elseif($square7 == $square8 && $square7 == $square9){
+						$result = $square7;
+					} else {
+						$result = "D";
+					}
+					
+					switch($result):
+						case "X":
+							++$this->xWins;
+							break;
+						case "O":
+							++$this->oWins;
+							break;
+						default:
+							++$this->draws;
+					endswitch;
+					
+					$insert = "INSERT INTO results(square1, square2, square3, square4, square5, square6, square7, square8, square9, result)" . 
+					"VALUES('$square1','$square2','$square3','$square4','$square5','$square6','$square7','$square8','$square9','$result')";
+					$result = $conn->query($insert);
+					if (!$result) die($conn->error);
+					$x = 0;
 				}
-				
-				switch($result):
-					case "X":
-						++$this->xWins;
-						break;
-					case "O":
-						++$this->oWins;
-						break;
-					default:
-						++$this->draws;
-				endswitch;
-				
-				$insert = "INSERT INTO results(square1, square2, square3, square4, square5, square6, square7, square8, square9, result)" . 
-				"VALUES('$square1','$square2','$square3','$square4','$square5','$square6','$square7','$square8','$square9','$result')";
-				$result = $conn->query($insert);
-				if (!$result) die($conn->error);
-				$x = 0;
-			}			
+			}
 		}
 	}
 
